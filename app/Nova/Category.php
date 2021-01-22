7c510efb6825655eb532d46f9c3894432a63def7
krunal dodiya <kunal.dodiya1@gmail.com>
2021-01-22 18:44:02 +0530

    added hasmany relation in categories and operators

--- app/Nova/Category.php
+++ app/Nova/Category.php
@@ -3,6 +3,7 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -46,6 +47,8 @@ class Category extends Resource
             ID::make(__('ID'), 'id')->sortable(),
 
             Text::make('Name'),
+
+            HasMany::make('Products', 'products', Product::class),
         ];
     }
 
