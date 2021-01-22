428e41acb1d53edb1ae390b3b509f62d29b0b442
krunal dodiya <kunal.dodiya1@gmail.com>
2021-01-22 11:07:29 +0530

    fixed product belongsto method error

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+use App\Models\Product;
+
+class OrderObserver
+{
+    /**
+     * Handle the Order "created" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function created(Order $order)
+    {
+        $product = Product::find($order->product_id);
+
+        $product->quantity = $product->quantity - $order->quantity;
+
+        $product->save();
+    }
+
+    /**
+     * Handle the Order "updated" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function updated(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "deleted" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function deleted(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "restored" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function restored(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "force deleted" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function forceDeleted(Order $order)
+    {
+        //
+    }
+}
